ff9878988f36cb0304bbe693851d72fc7b4f6e22
joomn11 <joomn11@gmail.com>
2022-11-25 10:06:20 +0900

    add 로그추적기

--- src/main/java/com/hello/advenced/AdvencedApplication.java
+++ src/main/java/com/hello/advenced/AdvencedApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdvencedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdvencedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdvencedApplication.class, args);
+    }
 
 }
