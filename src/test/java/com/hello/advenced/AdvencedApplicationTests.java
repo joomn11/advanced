c43ce8becaf20a3c4ddc97217ef7402e2171f045
joomn11 <joomn11@gmail.com>
2022-11-25 10:04:31 +0900

    add project

---
+++ src/test/java/com/hello/advenced/AdvencedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.advenced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvencedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
